886ab3b091cb9319b339216574761dd9db233c52
Cristian Bermudez <ccbabcn@gmail.com>
2022-05-22 20:00:58 +0200

    corrected logInForm import path on test

--- src/redux/components/LogInForm/LogInForm.test.js
+++ src/redux/components/LogInForm/LogInForm.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import store from "../../store/store";
-import LogInForm from "./LoginForm";
+import LogInForm from "./LogInForm";
 
 describe("Given a LogInForm component", () => {
   describe("When it's invoked", () => {
